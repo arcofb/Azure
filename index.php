2c11e0831330b29a29d7e18fa596ac8985c68d13
Arco <49058858+odakade@users.noreply.github.com>
2020-01-17 14:45:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -30,21 +30,13 @@
     $user = "arcos";
     $pass = "akucintaAllah01";
     $db = "arcos";
-    // PHP Data Objects(PDO) Sample Code:
-    try {
-      $conn = new PDO("sqlsrv:server = tcp:arcos.database.windows.net,1433; Database = arcos", "arcos", "akucintaAllah01");
-      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch (PDOException $e) {
-      print("Error connecting to SQL Server.");
-      die(print_r($e));
-    }
 
-   // SQL Server Extension Sample Code:
-     $connectionInfo = array("UID" => "arcos", "pwd" => "akucintaAllah01", "Database" => "arcos", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-     $serverName = "tcp:arcos.database.windows.net,1433";
-     $conn = sqlsrv_connect($serverName, $connectionInfo);
-    ?>
+    try {
+        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    } catch(Exception $e) {
+        echo "Failed: " . $e;
+    }
 
     if (isset($_POST['submit'])) {
         try {
