1b5755e1352a68a778a1b883f6d0e825f0d372a9
Arco <49058858+odakade@users.noreply.github.com>
2020-01-17 15:07:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "arcos.database.windows.net";
+    $user = "arcos";
+    $pass = "akucintaAllah01";
+    $db = "arcos";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
