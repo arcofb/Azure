3dee3422f216c1bbc63ab6cbc932e7689038e2b9
Arco <49058858+odakade@users.noreply.github.com>
2020-01-17 14:34:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -31,12 +31,21 @@
     $pass = "akucintaAllah01";
     $db = "arcos";
 
+    // PHP Data Objects(PDO) Sample Code:
     try {
-        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    } catch(Exception $e) {
-        echo "Failed: " . $e;
+      $conn = new PDO("sqlsrv:server = tcp:arcos.database.windows.net,1433; Database = arcos", "arcos", "{your_password_here}");
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
+    catch (PDOException $e) {
+      print("Error connecting to SQL Server.");
+      die(print_r($e));
+    }
+
+   // SQL Server Extension Sample Code:
+     $connectionInfo = array("UID" => "arcos", "pwd" => "{your_password_here}", "Database" => "arcos", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+     $serverName = "tcp:arcos.database.windows.net,1433";
+     $conn = sqlsrv_connect($serverName, $connectionInfo);
+    ?>
 
     if (isset($_POST['submit'])) {
         try {
