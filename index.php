73636149870993515bdccac0b259d697b350d43d
Arco <49058858+odakade@users.noreply.github.com>
2020-01-17 14:53:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
             $job = $_POST['job'];
             $date = date("Y-m-d");
             // Insert data
-            $sql_insert = "INSERT INTO Registri (name, email, job, date) 
+            $sql_insert = "INSERT INTO dbo.Registri (name, email, job, date) 
                         VALUES (?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
@@ -60,7 +60,7 @@
         echo "<h3>Your're registered!</h3>";
     } else if (isset($_POST['load_data'])) {
         try {
-            $sql_select = "SELECT * FROM Registri";
+            $sql_select = "SELECT * FROM dbo.Registri";
             $stmt = $conn->query($sql_select);
             $registrants = $stmt->fetchAll(); 
             if(count($registrants) > 0) {
