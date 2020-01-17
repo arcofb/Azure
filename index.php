a80b84401e85c97b12a7b9f407db57bc289f1a66
Arco <49058858+odakade@users.noreply.github.com>
2020-01-17 14:58:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
             $job = $_POST['job'];
             $date = date("Y-m-d");
             // Insert data
-            $sql_insert = "INSERT INTO dbo.Registri (name, email, job, date) 
+            $sql_insert = "INSERT INTO [dbo].[Registri] (name, email, job, date) 
                         VALUES (?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
@@ -74,7 +74,7 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
         echo "<h3>Your're registered!</h3>";
     } else if (isset($_POST['load_data'])) {
         try {
-            $sql_select = "SELECT * FROM dbo.Registri";
+            $sql_select = "SELECT * FROM [dbo].[Registri]";
             $stmt = $conn->query($sql_select);
             $registrants = $stmt->fetchAll(); 
             if(count($registrants) > 0) {
