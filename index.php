a0758dd6d0c6bfb79cc66c9feb2ced8c53b456df
Arco <49058858+odakade@users.noreply.github.com>
2020-01-17 15:30:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -20,14 +20,11 @@
  <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
  <form method="post" action="index.php" enctype="multipart/form-data" >
        <label>Nama</label><br>
-  					<input type="text" name="name" id="name" class="form-control" placeholder="Name"><br>
+  					<input type="text" name="name" id="name" class="form-control" placeholder="<- Your Name ->"><br>
        <label>Email</label><br>
-  					<input type="text" name="email" id="email" class="form-control" placeholder="Email"><br>
+  					<input type="text" name="email" id="email" class="form-control" placeholder="<- Your Email ->"><br>
        <label>Job</label><br>
-  					<input type="text" name="job" id="job" class="form-control" placeholder="Job"><br>
-<!--        Name  <input type="text" name="name" id="name"/></br></br> -->
-<!--        Email   <input type="text" name="email" id="email"/></br></br> -->
-<!--        Job <input type="text" name="job" id="job"/></br></br> -->
+  					<input type="text" name="job" id="job" class="form-control" placeholder="<- Your Job ->"><br>
        <input type="submit" name="submit" value="Submit" />
        <input type="submit" name="load_data" value="Load Data" />
  </form>
