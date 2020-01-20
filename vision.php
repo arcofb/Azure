5a9ededfc0bd4dbaad19cb01d8197207f9794e0b
Arco <49058858+odakade@users.noreply.github.com>
2020-01-21 00:06:47 +0700

    Update vision.php

--- vision.php
+++ vision.php
@@ -3,10 +3,10 @@ if (isset($_POST['submit'])) {
 	if (isset($_POST['url'])) {
 		$url = $_POST['url'];
 	} else {
-		header("Location: https://github.com/odakade/Azure/index.php");
+		header("Location: index.php");
 	}
 } else {
-	header("Location: https://github.com/odakade/Azure/index.php");
+	header("Location: index.php");
 }
 ?>
 
