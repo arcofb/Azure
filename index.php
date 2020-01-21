7f948b88ce7e9e5c9b35f55ea5f0c979a9f9143d
Arco <49058858+odakade@users.noreply.github.com>
2020-01-21 13:27:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link href="starter-template.css" rel="stylesheet">
   </head>
   <style type="text/css">
- 	/* body { background-color: #fff; border-top: solid 10px #000;
+ 	body { background-color: #fff; border-top: solid 10px #000;
  	    color: #333; font-size: .85em; margin: 20; padding: 20;
  	    font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
  	}
@@ -18,7 +18,7 @@
  	h3 { font-size: 1.2em; }
  	table { margin-top: 0.75em; }
  	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
- 	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; } */
+ 	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
      /* Add a black background color to the top navigation */
 .topnav {
   background-color: #333;
