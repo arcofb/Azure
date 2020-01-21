f5182b5646919c49d010b682bcd8bdd11500e6fa
Arco <49058858+odakade@users.noreply.github.com>
2020-01-21 12:50:31 +0700

    Update AZvision.php

--- AZvision.php
+++ AZvision.php
@@ -1,90 +1,96 @@
 <?php
 if (isset($_POST['submit'])) {
-	if (isset($_POST['url'])) {
-		$url = $_POST['url'];
-	} else {
-		header("Location: analisa.php");
-	}
+    if (isset($_POST['url'])) {
+        $url = $_POST['url'];
+    } else {
+        header("Location: analisa.php");
+    }
 } else {
-	header("Location: analisa.php");
+    header("Location: analisa.php");
 }
 ?>
 
 <!DOCTYPE html>
-    <html>
-    <head>
-    <title>Analisa Gambar dengan Azure Cognitive Service</title>
-    </head>
-        <body>
-        <div class="topnav">
-            <a href="http://arcosapp.azurewebsites.net/">Registri</a>
-            <a class="active" href="http://arcosapp.azurewebsites.net/analisa.php">Analisa Gambar</a>
-        </div>
-        <h1>Hasil Analisa</h1>
-        <script type="text/javascript">
-            $(document).ready(function () {
-            // **********************************************
-            // *** Update or verify the following values. ***
-            // **********************************************
-            // Replace <Subscription Key> with your valid subscription key.
-            var subscriptionKey = "371a5b82fd114364b68fddc33837286f";
-            // You must use the same Azure region in your REST API method as you used to
-            // get your subscription keys. For example, if you got your subscription keys
-            // from the West US region, replace "westcentralus" in the URL
-            // below with "westus".
-            //
-            // Free trial subscription keys are generated in the "westus" region.
-            // If you use a free trial subscription key, you shouldn't need to change
-            // this region.
-            var uriBase =
-            "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
-            // Request parameters.
-            var params = {
-                "visualFeatures": "Categories,Description,Color",
-                "details": "",
-                "language": "en",
-            };
-            // Display the image.
-            var sourceImageUrl = "<?php echo $url ?>";
-            document.querySelector("#sourceImage").src = sourceImageUrl;
-            // Make the REST API call.
-            $.ajax({
-                url: uriBase + "?" + $.param(params),
-                // Request headers.
-                beforeSend: function(xhrObj){
-                    xhrObj.setRequestHeader("Content-Type","application/json");
-                    xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-                },
-                type: "POST",
-                // Request body.
-                data: '{"url": ' + '"' + sourceImageUrl + '"}',
-            })
-            .done(function(data) {
-                // Show formatted JSON on webpage.
-                $("#responseTextArea").val(JSON.stringify(data, null, 2));
-                // console.log(data);
-                // var json = $.parseJSON(data);
-                $("#description").text(data.description.captions[0].text);
-            })
-            .fail(function(jqXHR, textStatus, errorThrown) {
-                // Display error message.
-                var errorString = (errorThrown === "") ? "Error. " :
-                errorThrown + " (" + jqXHR.status + "): ";
-                errorString += (jqXHR.responseText === "") ? "" :
-                jQuery.parseJSON(jqXHR.responseText).message;
-                alert(errorString);
-            });
-        });
+<html>
+<head>
+    <title>Hasil Analisa</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+
+    <script language="javascript">
+        document.getElementById('analyze_btn').click(); 
     </script>
-<br>
+
+</head>
+<body>
+ 
+<script type="text/javascript">
+    function processImage() {
+        
+        var subscriptionKey = "371a5b82fd114364b68fddc33837286f";
+ 
+        var uriBase =
+            "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
+ 
+        var params = {
+            "visualFeatures": "Categories,Description,Color",
+            "details": "",
+            "language": "en",
+        };
+ 
+        var sourceImageUrl = document.getElementById("inputImage").value;
+        document.querySelector("#sourceImage").src = sourceImageUrl;
+ 
+        // Make the REST API call.
+        $.ajax({
+            url: uriBase + "?" + $.param(params),
+ 
+            // Request headers.
+            beforeSend: function(xhrObj){
+                xhrObj.setRequestHeader("Content-Type","application/json");
+                xhrObj.setRequestHeader(
+                    "Ocp-Apim-Subscription-Key", subscriptionKey);
+            },
+ 
+            type: "POST",
+ 
+            // Request body.
+            data: '{"url": ' + '"' + sourceImageUrl + '"}',
+        })
+ 
+        .done(function(data) {
+            // Show formatted JSON on webpage.
+            $("#responseTextArea").val(JSON.stringify(data, null, 2));
+            $("#desc").text(data.description.captions[0].text);
+        })
+ 
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            // Display error message.
+            var errorString = (errorThrown === "") ? "Error. " :
+                errorThrown + " (" + jqXHR.status + "): ";
+            errorString += (jqXHR.responseText === "") ? "" :
+                jQuery.parseJSON(jqXHR.responseText).message;
+            alert(errorString);
+        });
+    };
+</script>
+ 
+<h1>Hasil Analisa</h1>
+<br><br>
+URL gambar:
+<input type="text" name="inputImage" id="inputImage"
+    value="<?php echo $url ?>" readonly />
+<button id="analyze_btn" onclick="processImage()">Analyze image</button>
+<br><br>
+<script language="javascript">
+document.getElementById('analyze_btn').click(); 
+</script>
 <div id="wrapper" style="width:1020px; display:table;">
-	<div id="imageDiv" style="width:420px; display:table-cell;">
-		<b>Source Image:</b>
-		<br><br>
-		<img id="sourceImage" width="400" />
-		<br>
-		<h3 id="description">Wait ..</h3>
-	</div>
+    <div id="imageDiv" style="width:420px; display:table-cell;">
+        Source image:
+        <br><br>
+        <img id="sourceImage" width="400" />
+        <h3 id="desc">Wait ..</h3>
+    </div>
 </div>
 </body>
 </html>
